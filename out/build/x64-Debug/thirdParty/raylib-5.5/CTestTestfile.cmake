2a39f2885d35e656c2b377cee9cb96eb8f5983ae
Hanweicarlao <carlos4349@gmail.com>
2026-04-21 09:08:28 +0800

    new changes

---
+++ out/build/x64-Debug/thirdParty/raylib-5.5/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Mac/Home/Documents/Codes/my-first-Cplusplus-game/thirdParty/raylib-5.5
+# Build directory: C:/Mac/Home/Documents/Codes/my-first-Cplusplus-game/out/build/x64-Debug/thirdParty/raylib-5.5
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
---
+++ src/platform/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <raylib.h>
+#include <imgui.h>
+#include <rlImGui.h>
+#include <gameMain.h>
+
+int main() {
+
+#if PRODUCTION_BUILD == 1
+	SetTraceLogLevel(LOG_NONE);//disable raylib logs in production build
+#endif
+
+
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(800, 450, "window name");
+
+#pragma region ImGui setup
+	rlImGuiSetup(true);
+
+	ImGuiIO& io = ImGui::GetIO();
+	io.FontGlobalScale = 2.5f;//you can make it bigger or smaller if you want
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;//enable docking
+	ImGui::StyleColorsClassic();
+#pragma endregion
+	if (!initGame())
+	{
+		//std::cerr << "Failed to initialize game!" << std::endl;
+		return 0;
+	}
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		
+#pragma region ImGui new frame
+		rlImGuiBegin();
+
+
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+		ImGui::PopStyleColor(2);
+
+		if (!updateGame())
+		{
+			CloseWindow();
+		}
+#pragma endregion
+
+
+		
+
+
+		rlImGuiEnd();
+		EndDrawing(); 
+
+	}
+	CloseWindow();
+	closeGame();
+
+#pragma region ImGui shutdown
+	rlImGuiShutdown();
+#pragma endregion
+	return 0;
+
+}
